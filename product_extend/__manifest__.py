8a14a660b6e7a50319479efe1cad6e53c636e8df
fa-andema <f.abachouch@andemaconsulting.com>
2019-10-28 10:10:06 +0100

    FIX product_extend manifest

--- product_extend/__manifest__.py
+++ product_extend/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Product extend",
     "version": "1.1",
-    "depends": ['base', 'product',],
+    "depends": ['base', 'product', 'sale'],
     "author": "ANDEMA",
     'website': 'http://www.andemaconulting.com',
     "category": "",
