89b53f412cf8c9b71fb146608eacfe903f512008
fa-andema <f.abachouch@andemaconsulting.com>
2019-10-28 12:01:03 +0100

    tag_parent

--- product_extend/models/pruduct_brand.py
+++ product_extend/models/pruduct_brand.py
@@ -10,8 +10,38 @@ class ProductBrand(models.Model):
 
 class ProductTags(models.Model):
      _name = 'product.tags'
+     _parent_name = "parent_id"
+     _parent_store = True
 
      name = fields.Char(required=True)
+     active = fields.Boolean(help='The active field allows you to hide the tag without removing it.', default=True)
+     parent_id = fields.Many2one(string='Parent', comodel_name='product.tags', index=True, select=True, ondelete='cascade')
+     parent_path = fields.Char(index=True)
+     display_name = fields.Char('Full Name', compute='_compute_display_name')
+
+     child_ids = fields.One2many(string='Child Tags', comodel_name='product.tags', inverse_name='parent_id')
+     parent_left = fields.Integer('Left Parent', select=True)
+     parent_right = fields.Integer('Right Parent', select=True)
+
+     @api.depends('name', 'parent_id.name')
+     def _compute_display_name(self):
+         """ Return the tags' display name, including their direct parent. """
+         for rec in self:
+             if rec.parent_id:
+                 rec.display_name = rec.parent_id.display_name + ' / ' + rec.name
+             else:
+                 rec.display_name = rec.name
+
+     @api.model
+     def name_search(self, name, args=None, operator='ilike', limit=100):
+         args = args or []
+         if name:
+             # Be sure name_search is symetric to name_get
+             name = name.split(' / ')[-1]
+             args = [('name', operator, name)] + args
+         tags = self.search(args, limit=limit)
+         return tags.name_get()
+
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
