33cfc55efd3261f0c2d8b41102db687b4c705333
fa-andema <f.abachouch@andemaconsulting.com>
2019-11-05 22:07:40 +0100

    stock_lot_extend

--- stock_lot_extend/models/stock_lot_extend.py
+++ stock_lot_extend/models/stock_lot_extend.py
@@ -6,8 +6,9 @@ from odoo import api, fields, models
 class StockProductionLot(models.Model):
     _inherit = 'stock.production.lot'
 
-    tag_ids = fields.Many2many('product.tags', 'Etiquette')
+    tag_ids = fields.Many2many('product.tags', relation='stock_production_lot_tag' ,string='Etiquette')
     latest_price = fields.Float('Montant achat')
     suggested_price = fields.Float(u'Prix conseillé')
     categ_id = fields.Many2one('product.category', related='product_id.categ_id', store=True)
+    occasion = fields.Boolean('Numero de serie Occasion')
     old_id = fields.Integer()
