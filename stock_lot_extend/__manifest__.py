cdca287d09c3c34bb40ef75e9acd114cd359e116
fa-andema <f.abachouch@andemaconsulting.com>
2019-11-05 20:34:50 +0100

    stock_lot_extend

---
+++ stock_lot_extend/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Gestion des numéro de série",
+    "version": "1.1",
+
+    "depends": ['base', 'product_extend', 'stock'],
+
+    "author": "ANDEMA",
+    'website': 'http://www.andemaconulting.com',
+    "category": "",
+    "description": "",
+    "init_xml": [],
+    'data': [
+     'views/stock_lot_views.xml',
+     # 'security/ir.model.access.csv',
+    ],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+}
