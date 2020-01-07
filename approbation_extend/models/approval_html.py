1583357c2db8ff5f298aca3cbc5c9cb68ec35825
fa-andema <f.abachouch@andemaconsulting.com>
2020-01-07 15:29:40 +0100

    APPROVALS

--- approbation_extend/models/approval_html.py
+++ approbation_extend/models/approval_html.py
@@ -5,4 +5,17 @@ from odoo import api, fields, models
 class ApprovalCategory(models.Model):
     _inherit = 'approval.category'
 
-    approbation = fields.Html('Approbation')
+    formulaire = fields.Html('Formulaire')
+
+
+class ApprovalRequest(models.Model):
+    _inherit = 'approval.request'
+
+
+    formulaire = fields.Html('Formulaire')
+
+    @api.onchange('category_id', 'request_owner_id')
+    def _onchange_category_id(self):
+        super(ApprovalRequest, self)._onchange_category_id()
+        if self.category_id.formulaire:
+            self.formulaire = self.category_id.formulaire
