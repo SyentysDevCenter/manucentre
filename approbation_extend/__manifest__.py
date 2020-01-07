d6a51cfcafc071c53802dc7da7d64d4e8b6c7d8c
fa-andema <f.abachouch@andemaconsulting.com>
2020-01-07 15:12:45 +0100

    commit

---
+++ approbation_extend/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Approval extend",
+    "version": "1.1",
+
+    "depends": ['base', 'approvals'],
+
+    "author": "ANDEMA",
+    'website': 'http://www.andemaconulting.com',
+    "category": "",
+    "description": "",
+    "init_xml": [],
+    'data': [
+        'views/approval_form_inherit.xml',
+    ],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+}
