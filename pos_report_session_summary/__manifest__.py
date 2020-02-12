0d4dde13c6d7741d50dd4d87300be93db3f97072
jpansyentys <jin.pan@syentys.fr>
2020-02-12 10:21:45 +0100

    [ADD]pos session summary report

---
+++ pos_report_session_summary/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'POS report Session Summary',
+
+    'summary': 'Session Summary report on the POS session',
+
+    'author': 'SYENTYS',
+
+    'depends': ['point_of_sale'],
+
+    'data': [
+        'views/session_summary_report.xml',
+        'views/report_session_summary.xml',
+    ],
+
+    'installable': True,
+    'application': False,
+}
